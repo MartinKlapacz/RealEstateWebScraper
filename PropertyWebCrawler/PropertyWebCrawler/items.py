e7a6dbc4b5cc06ef1f16c84aecd007db74513d6b
MartinKlapacz <klapacz.martin@gmail.com>
2019-10-10 13:21:07 +0200

    finished real estate page collection in parse method

--- PropertyWebCrawler/PropertyWebCrawler/items.py
+++ PropertyWebCrawler/PropertyWebCrawler/items.py
@@ -8,8 +8,7 @@
 import scrapy
 
 
-class PropertywebcrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
+class RealEstateItem(scrapy.Item):
     total_price = scrapy.Field()
     room_number = scrapy.Field()
     balcony = scrapy.Field()
@@ -25,4 +24,24 @@ class PropertywebcrawlerItem(scrapy.Item):
     energy_certificate = scrapy.Field()
     living_space = scrapy.Field()
     describtion = scrapy.Field()
+    
+'''    
+    def __init__(self):
+        total_price             = None
+        total_price             = None
+        room_number             = None
+        balcony                 = None
+        bedroom_number          = None
+        bathroom_number         = None
+        property_type           = None
+        garage                  = None
+        internetspeed_maximum   = None
+        construction_year       = None
+        object_condition        = None
+        heater                  = None
+        power_consumption       = None
+        energy_certificate      = None
+        living_space            = None
+        describtion             = None
+'''
     
--- PropertyWebCrawler/PropertyWebCrawler/spiders/PropertySpider.py
+++ PropertyWebCrawler/PropertyWebCrawler/spiders/PropertySpider.py
@@ -1,25 +1,32 @@
+
 # -*- coding: utf-8 -*-
 import scrapy
+from ..items import RealEstateItem
 
+#rename project to RealEstateScraper
 
-class PropertyspiderSpider(scrapy.Spider):
-    name = 'PropertySpider'
-    errors_counter = 0
+class PropertySpider(scrapy.Spider):
+    name = 'mainpage'
+    error_counter = 0
+    real_estate_link = 'https://www.immobilienscout24.de/expose/'
 
     def start_requests(self):
         urls = [
             'https://www.immobilienscout24.de/Suche/S-T/Wohnung-Kauf/Bayern/Muenchen-Kreis?enteredFrom=one_step_search',
-
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
-    def parse_ad(self, response):
+    def parse_real_estate_page(self, response):
+        # TODO: init exposeItem
+        realEstateItem = RealEstateItem()
         
-        pass
+        yield None
+
 
     def parse(self, response):
         data_object_ids = []
+        # TODO: get number of li-nodes inside the lu-node, instead of hardcoding 20
         for i in range(20):
             try:
                 query = '//*[@id="resultListItems"]/li[%s]/div/article' % i
@@ -27,23 +34,11 @@ class PropertyspiderSpider(scrapy.Spider):
                 data_object_id = resp.attrib['data-obid']
                 data_object_ids.append(data_object_id)
             except KeyError:
-                error_counter += 1
+                pass
+                # TODO: handle errors
+        
+        for data_object_id in data_object_ids:
+            next_expose = response.urljoin('/%s' % data_object_id)
+            yield scrapy.Request(next_expose, callback=self.parse_real_estate_page)
 
-        ad_urls = ['https://www.immobilienscout24.de/expose/%s/' for data_object_id in data_object_ids]
-
-        for ad_url in ad_urls:
-            yield scrapy.Request(url=ad_url, callback=self.parse_ad)
-    
-
-        
-        
-        
-        
-        # //*[@id="result-113562767"]/div[1]/a[2]
-        # //*[@id="result-113562767"]/div[1]/a[2]
-        # /html/body/div[1]/div/div[2]/div/div[3]/div[1]/div[1]/div/div/ul/li[1]/div/article/div[1]/a[2]
-
-        #//*[@id="resultListItems"]/li[2]/div
-        
-        
-        
+        # TODO: scrap next page, if it exists
